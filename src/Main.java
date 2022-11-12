cf707a09eb6c54ec70136c638e6d0eda11a32c39
Marker97 <chereche10@gmail.com>
2022-11-12 16:58:02 +0100

    If Statement_02

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String surname = "Marziano";
+
+        if(surname.contains("ni")){
+            System.out.println("Your surname contains the sequence of letters 'ni'");
+        }else {
+            System.out.println("Your surname doesn't contain the sequence of letters 'ni'");
+        }
+    }
+}
